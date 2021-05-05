dcb34a3f4d2cbddbab92edf71dd5093f15c95473
Marina734 <Mary_gr95@mail.ru>
2021-05-05 19:17:47 +0300

    creat repo

---
+++ js/index.js
@@ -0,0 +1,11 @@
+// (()=>
+// {
+//     $(window).on('scroll') , function(){
+//         if($('window').scrollTop() > 1600)
+//         {
+        
+//             $('.tovary').fadeIn(1000);
+//             if($(window).scrollTop() > 2200)
+//         }
+//     }
+// })();
